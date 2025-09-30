6b249793dd5195882aa75759b4b1d16271b65162
Anamika-m1 <216942anamika@gmail.com>
2025-09-30 10:13:30 +0530

    make some changes

--- app/routes/auth.$.jsx
+++ app/routes/auth.$.jsx
@@ -4,5 +4,7 @@ import { authenticate } from "../shopify.server";
 export const loader = async ({ request }) => {
   await authenticate.admin(request);
 
-  return redirect("/");
+  const url = new URL(request.url);
+  const redirectTo = url.searchParams.get('redirect') || '/app';
+  return redirect(redirectTo);
 };
