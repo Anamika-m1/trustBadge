a4a4dd46efd96a9ac224c545bd783a832a2bf339
Anamika-m1 <216942anamika@gmail.com>
2025-09-30 09:42:58 +0530

    make some changes

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -22,7 +22,7 @@ export default function App() {
         <Link to="/app" rel="home">
           Home
         </Link>
-        {/* <Link to="/app/trustBadge">Trust Badge</Link> */}
+        <Link to="/app/additional">Trust Badge</Link>
       </NavMenu>
       <Outlet />
     </AppProvider>
